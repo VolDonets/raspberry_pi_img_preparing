8141ce0589b4821adee73686eef0619f7b5f13e2
VolDonets <vovan.s.marsa@gmail.com>
2020-10-20 17:27:26 +0300

    A few commands for installing cmake, c/c++ compiler and so on

---
+++ install_cpp_stuff.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt-get install build-essential -y
+sudo apt-get install checkinstall -y
+sudo apt-get install cmake -y
+sudo apt-get install pkg-config -y
+sudo apt-get install yasm -y
+sudo apt-get install unzip -y
