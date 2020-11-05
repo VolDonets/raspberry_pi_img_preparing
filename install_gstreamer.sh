65f20e90abd396f4905affb1dad68b6fe5441fc8
Donets Vladimir <vovan.s.marsa@gmail.com>
2020-11-05 13:28:48 +0200

    Update install_gstreamer.sh
    I have added a '-y' option for automatic command executing.

--- install_gstreamer.sh
+++ install_gstreamer.sh
@@ -9,15 +9,15 @@ sudo apt-get install libgstreamer1.0-dev -y
 # this one contains webrtc plugin for the gst pipeline
 sudo apt-get install libgstreamer-plugins-bad1.0-dev -y
 
-sudo apt-get install gstreamer1.0-plugins-base
-sudo apt-get install gstreamer1.0-libav
-sudo apt-get install gstreamer1.0-doc
-sudo apt-get install gstreamer1.0-tools
-sudo apt-get install gstreamer1.0-x 
-sudo apt-get install gstreamer1.0-alsa
-sudo apt-get install gstreamer1.0-gl
-sudo apt-get install gstreamer1.0-gtk3
-sudo apt-get install gstreamer1.0-pulseaudio
+sudo apt-get install gstreamer1.0-plugins-base -y
+sudo apt-get install gstreamer1.0-libav -y
+sudo apt-get install gstreamer1.0-doc -y
+sudo apt-get install gstreamer1.0-tools -y
+sudo apt-get install gstreamer1.0-x -y
+sudo apt-get install gstreamer1.0-alsa -y
+sudo apt-get install gstreamer1.0-gl -y
+sudo apt-get install gstreamer1.0-gtk3 -y
+sudo apt-get install gstreamer1.0-pulseaudio -y
 
 
 # This one used for woring an WEBRTC code on C++
@@ -28,10 +28,10 @@ sudo apt-get install libjson-glib-dev -y
 # This one used in OpenCV lib
 sudo apt-get install libgstreamer0.10-dev -y
 sudo apt-get install libgstreamer-plugins-base0.10-dev -y
-sudo apt-get install libgstreamer-plugins-base1.0-dev
+sudo apt-get install libgstreamer-plugins-base1.0-dev -y
 
 # This one used for installing 
-sudo apt-get isntall gstreamer1.0-omx -y
+sudo apt-get install gstreamer1.0-omx -y
 
 # Updating openssl configuration for posibility to work with WEBRTC connection
 echo "export OPENSSL_CONF=\"\"" >> ~/.bashrc
