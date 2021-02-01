2a53c60d5d0f73998436f8c485728bb651a15aca
Volodymyr Donets <vovan.s.marsa@gmail.com>
2021-02-01 17:01:19 +0000

    Added possibility to configure wi-fi name and pass without changing script lines

--- set_wifi_access_point.sh
+++ set_wifi_access_point.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+if [ "$1" == "" ] | [ "$2" == "" ]; then
+   echo "WARNING!!! you should set parameters for this script:"
+   echo "the fist is WIFI_ACCESS_POINT_NAME"
+   echo "the second is WIFI_ACCESS_POINT_PASSWORD"
+   exit 1
+else
+   if [ ${#2} -le 7 ]; then
+      echo "WiFi PASSWORD is too short, it should be 8 or greater"
+      exit 1
+   fi
+   echo "WiFi name: $1"
+   echo "WiFi pass: $2"
+fi
+
+
 # Install NetworkManager, now it doesn't available on the clean RaspbianOS
 sudo apt-get install network-manager -y
 sudo apt-get install dnsmasq -y
@@ -20,18 +35,17 @@ sudo echo ""                         >> /etc/NetworkManager/NetworkManager.conf
 sudo echo "[ifupdown]"               >> /etc/NetworkManager/NetworkManager.conf
 sudo echo "managed=false"            >> /etc/NetworkManager/NetworkManager.conf
 
-# for testing
-# nmcli dev wifi hotspot ifname wlan0 ssid IronRPI password "test1234"
-nmcli con add type wifi ifname wlan0 mode ap con-name IRON_AP ssid IRON_AP
-nmcli con modify IRON_AP 802-11-wireless.band bg
-nmcli con modify IRON_AP 802-11-wireless.channel 1
-nmcli con modify IRON_AP 802-11-wireless-security.key-mgmt wpa-psk
-nmcli con modify IRON_AP 802-11-wireless-security.proto rsn
-nmcli con modify IRON_AP 802-11-wireless-security.group ccmp
-nmcli con modify IRON_AP 802-11-wireless-security.pairwise ccmp
-nmcli con modify IRON_AP 802-11-wireless-security.psk 11223344
-nmcli con modify IRON_AP ipv4.method shared
-nmcli con up IRON_AP
+# for configuring wi-fi ap, name, pass, and other wi-if stuff
+nmcli con add type wifi ifname wlan0 mode ap con-name "$1" ssid "$1"
+nmcli con modify "$1" 802-11-wireless.band bg
+nmcli con modify "$1" 802-11-wireless.channel 1
+nmcli con modify "$1" 802-11-wireless-security.key-mgmt wpa-psk
+nmcli con modify "$1" 802-11-wireless-security.proto rsn
+nmcli con modify "$1" 802-11-wireless-security.group ccmp
+nmcli con modify "$1" 802-11-wireless-security.pairwise ccmp
+nmcli con modify "$1" 802-11-wireless-security.psk "$2"
+nmcli con modify "$1" ipv4.method shared
+nmcli con up "$1"
 
 
 
