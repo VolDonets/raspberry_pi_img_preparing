ae22bb98ff3ef62e7d569a7aec29a150ddf1e121
Vladimir Donets <vovan.s.marsa@gmail.com>
2021-02-02 16:15:10 +0000

    fixed access point enabling

--- set_wifi_access_point.sh
+++ set_wifi_access_point.sh
@@ -14,9 +14,12 @@ else
    echo "WiFi pass: $2"
 fi
 
+sleep 2
 
 # Install NetworkManager, now it doesn't available on the clean RaspbianOS
+sudo apt-get update
 sudo apt-get install network-manager -y
+sudo apt-get install network-manager-gnome -y
 sudo apt-get install dnsmasq -y
 
 # Disabling standalone dnsmasq service
@@ -27,13 +30,30 @@ sudo apt-get install dnsmasq -y
 sudo systemctl disable dnsmasq
 sudo systemctl stop dnsmasq
 
+sleep 1
+
 # For NetworkManager to run dnsmasq as a local caching DNS server
 sudo echo "[main]"                    > /etc/NetworkManager/NetworkManager.conf
 sudo echo "plugins=ifupdown,keyfile" >> /etc/NetworkManager/NetworkManager.conf
 sudo echo "dns=dnsmasq"              >> /etc/NetworkManager/NetworkManager.conf
+sudo echo "dhcp=internal"            >> /etc/NetworkManager/NetworkManager.conf
 sudo echo ""                         >> /etc/NetworkManager/NetworkManager.conf
 sudo echo "[ifupdown]"               >> /etc/NetworkManager/NetworkManager.conf
-sudo echo "managed=false"            >> /etc/NetworkManager/NetworkManager.conf
+sudo echo "managed=true"             >> /etc/NetworkManager/NetworkManager.conf
+
+# disable wpa managing for wlan0
+sudo echo "denyinterfaces wlan0"     > /etc/dhcpd.conf
+
+# restart network-manager
+sudo systemctl restart network-manager.service
+
+
+# print information about available devices
+echo "\n\n <----------------------------------------> \n\n"
+sudo nmcli dev
+echo "\n\n <----------------------------------------> \n\n"
+
+sleep 1
 
 # for configuring wi-fi ap, name, pass, and other wi-if stuff
 nmcli con add type wifi ifname wlan0 mode ap con-name "$1" ssid "$1"
@@ -47,5 +67,11 @@ nmcli con modify "$1" 802-11-wireless-security.psk "$2"
 nmcli con modify "$1" ipv4.method shared
 nmcli con up "$1"
 
-
-
+# reboot raspberry pi
+read -p "Reboot now Y/n: " USER_CHOISE
+if [ "$USER_CHOISE" == "y"] | [ "$USER_CHOISE" == "Y" ] | [ "$USER_CHOISE" == "" ]; then
+   echo "Rebooting ..."
+   sudo reboot
+else
+   echo "Please REBOOT whet it'll be comfortable"
+fi
